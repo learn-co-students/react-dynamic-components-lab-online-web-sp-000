12142884c4556eea06f21803dbd8250219ce7d1e
Patrice Drayton <draytonpatrice@marilynssecret.org>
2019-06-21 13:04:26 -0400

    Done.

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import React, { Component } from 'react';
 import BlogPost from './BlogPost.js'
 import ColorBox from './ColorBox.js'
 
+
 class App extends Component {
-  
-  // Nothing needs to change here! 
+
+  // Nothing needs to change here!
   // Make note of what prop we are initially passing to `ColorBox` below!
-  
+
   render() {
     return (
       <div id="app">
@@ -14,6 +15,7 @@ class App extends Component {
           <div id="seperator"></div>
           <div className="wrapper">
             <ColorBox opacity={1} />
+
           </div>
       </div>
     )

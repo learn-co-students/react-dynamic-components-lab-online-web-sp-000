dc042284b0fae8e3cad76228f582ab2ffec76234
treuther <tracyreuther@gmail.com>
2020-01-26 16:24:09 -0600

    Done.

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -4,8 +4,12 @@ export default class ColorBox extends Component {
 
   render() {
     return (
-      <div className="color-box" style={{opacity: 2}}>
+      <div className="color-box" style={{opacity: this.props.opacity}}>
         {/* your conditional code here! */}
+        {/*if the opacity is greater than or equal to 0.2*/}
+          {/*render another ColorBox, pass opacity to child, reduct opacity by 0.1*/}
+          {/*otherwise, do NOT render another ColorBox and the return should be null*/}
+        {this.props.opacity >= 0.2 ? <ColorBox opacity={this.props.opacity - 0.1} /> : null}
       </div>
     )
   }

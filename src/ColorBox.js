0c0caa00818efb24b794616c2a0bef006d28bf9f
github username <github email address>
2020-05-20 20:22:13 -0600

    Done.

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -9,6 +9,11 @@ export default class ColorBox extends Component {
   }
 
   render() {
-        return this.props.opacity >= 0.2 ? (<div className="color-box" style={{opacity: this.props.opacity}}> <ColorBox opacity={(this.props.opacity - 0.1)} /> </div>) : null
+        console.log(this.props.opacity)
+        return (
+        <div className="color-box" style={{opacity: this.props.opacity}}>
+        {this.props.opacity >= 0.2 ? (<ColorBox opacity={(10*(this.props.opacity) - (10*0.1))/10} />) : null}
+        </div>
+        )
   }
 }

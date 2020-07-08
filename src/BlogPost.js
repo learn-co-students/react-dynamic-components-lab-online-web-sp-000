c4e795684852a89dc0bf09d85a9eb1a8823d7154
Soner Mezgitci <mezgitci9@gmail.com>
2020-07-08 00:46:55 -0400

    Done.

--- src/BlogPost.js
+++ src/BlogPost.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
-// are we missing an import?
+import Comment from './Comment.js'
 
-export default class BlogPost extends Component {
+export default class BlogPost extends React.Component {
   render() {
     
     const comments = [
@@ -18,11 +18,11 @@ export default class BlogPost extends Component {
         Still I'll rise.<br/>
         -Maya Angelou<br/>
 
-        {/* (remove the comment ticks and their surrounding brackets)
+       
         <Comment commentText={comments[0]} />
-        <Comment commentText={comments[0]} />
-        <Comment commentText={comments[0]} />
-        */}
+        <Comment commentText={comments[1]} />
+        <Comment commentText={comments[2]} />
+       
       </div>
     );
   }

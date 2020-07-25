5ab422c941024f4459c875682fc84466737ae6d6
dayofgray <graysonpberger@gmail.com>
2020-07-25 10:06:51 -0400

    Revert "Done."
    This reverts commit bfc599979a1be92d56b006cebbd650514782cbde.

--- src/Comment.js
+++ src/Comment.js
@@ -1,17 +1,1 @@
-import React, { Component } from 'react'
-
-class Comment extends Component {
-
-  render () {
-    return( 
-    <div className="comment">
-
-        {this.props.commentText}
-
-    </div>
-    )
-  }
-
-}
-
-export default Comment;
+//your code here

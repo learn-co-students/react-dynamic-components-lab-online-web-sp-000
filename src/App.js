52f60e724bebfc0b7458bf6da49809656fcefc24
Josh Ellis <joshe523@gmail.com>
2020-07-30 15:38:37 -0500

    Done.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import BlogPost from './BlogPost.js'
 import ColorBox from './ColorBox.js'
 
 class App extends Component {
-  
-  // Nothing needs to change here! 
+  // Nothing needs to change here!
   // Make note of what prop we are initially passing to `ColorBox` below!
-  
-  render() {
+
+  render () {
     return (
-      <div id="app">
-          <BlogPost />
-          <div id="seperator"></div>
-          <div className="wrapper">
-            <ColorBox opacity={1} />
-          </div>
+      <div id='app'>
+        <BlogPost />
+        <div id='seperator'></div>
+        <div className='wrapper'>
+          <ColorBox opacity={1} />
+        </div>
       </div>
     )
   }
 }
 
-export default App;
+export default App
--- src/ColorBox.js
+++ src/ColorBox.js
@@ -1,20 +1,20 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 
 export default class ColorBox extends Component {
-
   state = {
-    todos: [
-      
-    ]
+    todos: []
   }
 
-  render() {
+  render () {
+    const recursive = () => {
+      if (this.props.opacity >= 0.2) {
+        return <ColorBox opacity={this.props.opacity - 0.1} />
+      }
+    }
     return (
-      <div className="color-box" style={{opacity: null /*replace null with the value*/}}>
-        {/* your conditional code here! */}
+      <div className='color-box' style={{ opacity: this.props.opacity }}>
+        {recursive()}
       </div>
     )
   }
-
 }
-

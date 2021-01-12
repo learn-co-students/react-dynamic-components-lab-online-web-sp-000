e7dc65b15da2e6680e72387669fd3780fa370fb2
jenleebeaver <jen.lee.beaver@gmail.com>
2021-01-12 10:37:28 -0800

    Done.

--- src/Comment.js
+++ src/Comment.js
@@ -1,1 +1,16 @@
-//your code here
+
+import React, { Component } from 'react'
+import BlogPost from './BlogPost'
+
+export default class Comment extends 
+    React.Component {
+        render() {
+            return (
+                <div>
+                    <BlogPost className= {
+                        "comment"
+                    }/>
+                </div>
+            )
+        }
+    }
